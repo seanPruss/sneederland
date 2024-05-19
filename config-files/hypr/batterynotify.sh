11a3ab742df631ce282f5685ae42577ba84a5d7d
seanPruss <seanpruss5@gmail.com>
2024-05-18 21:36:12 -0400

    battery notifications every 10 minutes instead of 20 when battery high or low

--- config-files/hypr/batterynotify.sh
+++ config-files/hypr/batterynotify.sh
@@ -3,10 +3,10 @@ while true; do
 	bat_lvl=$(cat /sys/class/power_supply/BAT0/capacity)
 	if [ "$bat_lvl" -le 25 ]; then
 		notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
-		sleep 1200
+		sleep 600
 	elif [[ "$bat_lvl" -gt 95 ]]; then
 		notify-send --urgency=LOW "Battery High" "Level: ${bat_lvl}%"
-		sleep 1200
+		sleep 600
 	else
 		sleep 120
 	fi
