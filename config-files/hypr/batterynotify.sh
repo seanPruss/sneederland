05d8a257c28e7358015ab7b17b19182b8170b601
seanPruss <seanpruss5@gmail.com>
2024-05-20 19:57:10 -0400

    batterynotify.sh back to what it was before

--- config-files/hypr/batterynotify.sh
+++ config-files/hypr/batterynotify.sh
@@ -1,38 +1,13 @@
-#!/bin/sh
-
-# Runs with an exec-once when hyprland is launched I hope this works lol
-
-# Battery percentage at which to notify
-WARNING_LEVEL=20
-CRITICAL_LEVEL=5
-
-# Use files to store whether we've shown a notification or not (to prevent multiple notifications)
-FULL_FILE=/tmp/batteryfull
-EMPTY_FILE=/tmp/batteryempty
-CRITICAL_FILE=/tmp/batterycritical
-
+#!/bin/bash
 while true; do
-	BATTERY_DISCHARGING=$(acpi -b | grep "Battery 0" | grep -c "Discharging")
-	BATTERY_LEVEL=$(acpi -b | grep "Battery 0" | grep -P -o '[0-9]+(?=%)')
-	# Reset notifications if the computer is charging/discharging
-	if [ "$BATTERY_DISCHARGING" -eq 1 ] && [ -f $FULL_FILE ]; then
-		rm $FULL_FILE
-	elif [ "$BATTERY_DISCHARGING" -eq 0 ] && [ -f $EMPTY_FILE ]; then
-		rm $EMPTY_FILE
+	bat_lvl=$(cat /sys/class/power_supply/BAT0/capacity)
+	if [ "$bat_lvl" -le 25 ]; then
+		notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
+		sleep 600
+	elif [[ "$bat_lvl" -gt 95 ]]; then
+		notify-send --urgency=LOW "Battery High" "Level: ${bat_lvl}%"
+		sleep 600
+	else
+		sleep 120
 	fi
-
-	# If the battery is charging and is full (and has not shown notification yet)
-	if [ "$BATTERY_LEVEL" -gt 95 ] && [ "$BATTERY_DISCHARGING" -eq 0 ] && [ ! -f $FULL_FILE ]; then
-		notify-send "Battery Charged" "Battery is fully charged." -i "battery" -r 9991
-		touch $FULL_FILE
-		# If the battery is low and is not charging (and has not shown notification yet)
-	elif [ "$BATTERY_LEVEL" -le $WARNING_LEVEL ] && [ "$BATTERY_DISCHARGING" -eq 1 ] && [ ! -f $EMPTY_FILE ]; then
-		notify-send "Low Battery" "${BATTERY_LEVEL}% of battery remaining." -u critical -i "battery-alert" -r 9991
-		touch $EMPTY_FILE
-		# If the battery is critical and is not charging (and has not shown notification yet)
-	elif [ "$BATTERY_LEVEL" -le $CRITICAL_LEVEL ] && [ "$BATTERY_DISCHARGING" -eq 1 ] && [ ! -f $CRITICAL_FILE ]; then
-		notify-send "Battery Critical" "The computer will shutdown soon." -u critical -i "battery-alert" -r 9991
-		touch $CRITICAL_FILE
-	fi
-	sleep 300
 done
