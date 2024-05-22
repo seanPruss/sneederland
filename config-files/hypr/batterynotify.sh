a56c524df8ddbf63d48ca9091b232d00be168429
seanPruss <seanpruss5@gmail.com>
2024-05-22 12:08:31 -0400

    account for charging/discharging in batterynotify.sh

--- config-files/hypr/batterynotify.sh
+++ config-files/hypr/batterynotify.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 while true; do
-	bat_lvl=$(cat /sys/class/power_supply/BAT0/capacity)
-	if [ "$bat_lvl" -le 25 ]; then
-		notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
-		sleep 600
-	elif [[ "$bat_lvl" -gt 95 ]]; then
-		notify-send --urgency=LOW "Battery High" "Level: ${bat_lvl}%"
-		sleep 600
+	BATTERY_DISCHARGING=$(acpi -b | grep "Battery 0" | grep -c "Discharging")
+	BATTERY_LEVEL=$(acpi -b | grep "Battery 0" | grep -P -o '[0-9]+(?=%)')
+	if [ "$BATTERY_LEVEL" -le 25 ] && [ "$BATTERY_DISCHARGING" -eq 1 ]; then
+		notify-send --urgency=CRITICAL "Battery Low" "Level: ${BATTERY_LEVEL}%"
+		sleep 300
+	elif [ "$BATTERY_LEVEL" -gt 95 ] && [ "$BATTERY_DISCHARGING" -eq 0 ]; then
+		notify-send --urgency=LOW "Battery High" "Level: ${BATTERY_LEVEL}%"
+		sleep 300
 	else
 		sleep 120
 	fi
