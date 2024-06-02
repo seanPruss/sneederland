7c21564f61e24e0ad83c236b7a4ac2749982bf31
seanPruss <seanpruss5@gmail.com>
2024-06-02 19:41:12 -0400

    make sure gammastep service starts

--- config-files/hypr/batterynotify.sh
+++ config-files/hypr/batterynotify.sh
@@ -3,10 +3,10 @@ while true; do
 	BATTERY_DISCHARGING=$(acpi -b | grep "Battery 0" | grep -c "Discharging")
 	BATTERY_LEVEL=$(acpi -b | grep "Battery 0" | grep -P -o '[0-9]+(?=%)')
 	if [ "$BATTERY_LEVEL" -le 25 ] && [ "$BATTERY_DISCHARGING" -eq 1 ]; then
-		notify-send --urgency=CRITICAL "Battery Low" "Level: ${BATTERY_LEVEL}%"
+		notify-send --urgency=CRITICAL "󱊡 Battery Low" "Level: ${BATTERY_LEVEL}%"
 		sleep 300
 	elif [ "$BATTERY_LEVEL" -gt 95 ] && [ "$BATTERY_DISCHARGING" -eq 0 ]; then
-		notify-send --urgency=LOW "Battery High" "Level: ${BATTERY_LEVEL}%"
+		notify-send --urgency=LOW "󰁹 Battery High" "Level: ${BATTERY_LEVEL}%"
 		sleep 300
 	else
 		sleep 120
