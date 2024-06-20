2ba25a7ef2335419d5d101d784ffb0aef7cd90ee
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-06-20 09:49:00 -0400

    fix waybar spotify script

--- config-files/waybar/scripts/mediaplayer.sh
+++ config-files/waybar/scripts/mediaplayer.sh
@@ -6,11 +6,12 @@ while true; do
 	if [ "$player_status" = "Playing" ]; then
 		artist=$(playerctl -p spotify metadata artist)
 		title=$(playerctl -p spotify metadata title)
+		# Escape special characters for JSON
+		artist=$(echo "$artist" | sed 's/&/&amp;/g')
+		title=$(echo "$title" | sed 's/&/&amp;/g')
 		echo '{"text": "'"$artist - $title"'", "class": "custom-spotify", "alt": "Spotify"}'
 	elif [ "$player_status" = "Paused" ]; then
-		artist=$(playerctl -p spotify metadata artist)
-		title=$(playerctl -p spotify metadata title)
-		echo '{"text": "'"Paused"'", "class": "custom-spotify", "alt": "Spotify (Paused)"}'
+		echo '{"text": "Paused", "class": "custom-spotify", "alt": "Spotify (Paused)"}'
 	fi
 	sleep 3
 done
