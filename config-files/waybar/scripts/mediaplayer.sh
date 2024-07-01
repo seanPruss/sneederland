01654bd3b1d38d256ebe40c001993921d7f10228
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-07-01 19:30:06 -0400

    switch to betterbird

--- config-files/waybar/scripts/mediaplayer.sh
+++ config-files/waybar/scripts/mediaplayer.sh
@@ -4,11 +4,8 @@ while true; do
 	player_status=$(playerctl -p spotify status 2>/dev/null)
 
 	if [ "$player_status" = "Playing" ]; then
-		artist=$(playerctl -p spotify metadata artist)
-		title=$(playerctl -p spotify metadata title)
-		# Escape special characters for JSON
-		artist=$(echo "$artist" | sed 's/&/&amp;/g')
-		title=$(echo "$title" | sed 's/&/&amp;/g')
+		artist=$(playerctl -p spotify metadata artist | sed 's/&/&amp;/g')
+		title=$(playerctl -p spotify metadata title | sed 's/&/&amp;/g')
 		echo '{"text": "'"$artist - $title"'", "class": "custom-spotify", "alt": "Spotify"}'
 	elif [ "$player_status" = "Paused" ]; then
 		echo '{"text": "Paused", "class": "custom-spotify", "alt": "Spotify (Paused)"}'
