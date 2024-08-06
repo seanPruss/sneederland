0c31a5675def14974ddd8c80c9e4cd803c121f3a
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-08-05 20:24:53 -0400

    stupid dollar sign

--- installer.sh
+++ installer.sh
@@ -173,7 +173,7 @@ CER="[\e[1;31mERROR\e[0m]"
 CAT="[\e[1;37mATTENTION\e[0m]"
 CWR="[\e[1;35mWARNING\e[0m]"
 CAC="[\e[1;33mACTION\e[0m]"
-INSTLOG=REPO_DIR/install.log
+INSTLOG=$REPO_DIR/install.log
 
 ######
 # functions go here
