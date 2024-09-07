c943270fe3896e444a2c3d8cb5f5c60ac99cf354
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-09-07 19:49:47 -0400

    this should work and is hopefully better

--- config-files/hypr/start-gammastep.sh
+++ config-files/hypr/start-gammastep.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 while true; do
-	systemctl --user enable --now geoclue-agent.service
-	systemctl --user enable --now gammastep.service
-	sleep 60
+	if ! systemctl --user status gammastep; then
+		systemctl --user enable --now geoclue-agent.service
+		systemctl --user enable --now gammastep.service
+		sleep 60
+	fi
 done
