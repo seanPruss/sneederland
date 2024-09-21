d6c8f7cf0a094f2dbefe1f838bea18a74f17cc31
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-09-21 15:41:05 -0400

    balls

--- config-files/hypr/change-wallpaper.sh
+++ config-files/hypr/change-wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
-	WALLPAPER="$(fd -a . $HOME/.config/hypr/wallpapers | sort -R | head -1)"
+	WALLPAPER="$(fd -a . ~/.config/hypr/wallpapers | sort -R | head -1)"
 	cp -f $WALLPAPER ~/.cache/current_wallpaper
 	swww img --transition-type any "$WALLPAPER"
-	sleep 600
+	sleep 900
 done
--- config-files/hypr/start-gammastep.sh
+++ config-files/hypr/start-gammastep.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 systemctl --user enable --now geoclue-agent.service
 systemctl --user enable --now gammastep.service
-while true; do
-	if ! systemctl --user status gammastep; then
-		systemctl --user enable --now geoclue-agent.service
-		systemctl --user enable --now gammastep.service
-		sleep 60
-	fi
+while ! systemctl --user status gammastep; do
+	systemctl --user enable --now geoclue-agent.service
+	systemctl --user enable --now gammastep.service
+	sleep 60
 done
