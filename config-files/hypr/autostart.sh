ddbf8c9bfb80530882f243756061e622465e096a
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-09-22 19:11:41 -0400

    nuts

--- config-files/hypr/autostart.sh
+++ config-files/hypr/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+hyprctl monitors | grep HDMI-A-1 && hyprctl dispatch workspace 2
 while ! ping -c 1 archlinux.org; do
 	true
 done
--- config-files/hypr/change-wallpaper.sh
+++ config-files/hypr/change-wallpaper.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-while true; do
-	WALLPAPER="$(fd -a . ~/.config/hypr/wallpapers | sort -R | head -1)"
+
+update-wallpaper() {
+	WALLPAPER="$(fd -a . ~/.config/hypr/wallpapers | shuf -n 1)"
 	cp -f $WALLPAPER ~/.cache/current_wallpaper
 	swww img --transition-type any "$WALLPAPER"
+}
+
+while true; do
+	update-wallpaper
 	sleep 900
 done
