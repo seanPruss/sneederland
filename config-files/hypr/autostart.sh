7e22a5922c256ba21d82fcd3339c876c128749e2
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-09-24 18:20:00 -0400

    balls

--- config-files/hypr/autostart.sh
+++ config-files/hypr/autostart.sh
@@ -2,9 +2,9 @@
 gsettings set org.gnome.desktop.interface cursor-theme BreezeX-RosePine-Linux
 gsettings set org.gnome.desktop.interface cursor-size 25
 hyprctl monitors | grep HDMI-A-1 && hyprctl dispatch workspace 2
-hyprctl dispatch exec kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/pipes-rs.sh"
-hyprctl dispatch exec kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/cmatrix.sh"
-hyprctl dispatch exec kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/cava.sh"
+kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/pipes-rs.sh" &
+kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/cmatrix.sh" &
+kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/cava.sh" &
 while ! ping -c 1 archlinux.org; do
 	true
 done
