443d09d3634a384fdea8ce23825800c2ea9f8896
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-10-08 10:29:39 -0400

    update hyprpm in autostart.sh

--- config-files/hypr/autostart.sh
+++ config-files/hypr/autostart.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+hyprwinwrap-background() {
+	kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/pipes-rs.sh" &
+	kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/cmatrix.sh" &
+	kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/cava.sh" &
+}
+
+update-hyprpm() {
+	hyprpm update && hyprwinwrap-background
+}
+
 swaync &
 waybar &
 swww-daemon &
@@ -6,12 +17,10 @@ hyprctl setcursor rose-pine-hyprcursor 24
 gsettings set org.gnome.desktop.interface cursor-theme BreezeX-RosePine-Linux
 gsettings set org.gnome.desktop.interface cursor-size 24
 hyprctl monitors | grep HDMI-A-1 && hyprctl dispatch workspace 2
-kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/pipes-rs.sh" &
-kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/cmatrix.sh" &
-kitty -c "$HOME/.config/kitty/kittyconfigbg.conf" --class="kitty-bg" "$HOME/.config/hypr/cava.sh" &
 while ! ping -c 1 archlinux.org; do
 	true
 done
+update-hyprpm &
 tldr --update &
 hyprctl dispatch exec spotify
 hyprctl dispatch exec vesktop
