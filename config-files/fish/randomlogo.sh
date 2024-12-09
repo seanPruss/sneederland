446f17103879efcd4958bc0e0d8626a660926703
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-12-09 09:35:33 -0500

    balls

--- config-files/fish/randomlogo.sh
+++ config-files/fish/randomlogo.sh
@@ -7,7 +7,7 @@ if [ $rand -eq 0 ]; then
 elif [ $rand -eq 1 ]; then
 	colorscript random
 elif [ $rand -eq 2 ]; then
-	toilet -t -f fraktur 'I use Arch btw'
+	toilet -t -f doom -F metal 'I use Arch btw'
 else
-	toilet -t -f fraktur 'The Sneed Machine'
+	toilet -t -f doom -F metal 'The Sneed Machine'
 fi
