8c0463fd382a515d2c80ceac7413052c721fbc89
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-12-09 20:21:20 -0500

    forgot this kek

--- config-files/hypr/start-gammastep.sh
+++ config-files/hypr/start-gammastep.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-systemctl --user enable --now geoclue-agent.service
 systemctl --user enable --now gammastep.service
 while ! systemctl --user status gammastep; do
-	systemctl --user enable --now geoclue-agent.service
 	systemctl --user enable --now gammastep.service
 done
