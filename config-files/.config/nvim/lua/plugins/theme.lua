9499a7cd96d0a10846e5c46ede710de117e95e42
seanPruss <36770417+seanPruss@users.noreply.github.com>
2024-12-29 14:57:23 -0500

    balls

--- config-files/.config/nvim/lua/plugins/theme.lua
+++ config-files/.config/nvim/lua/plugins/theme.lua
@@ -2,6 +2,12 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
+        opts = {
+            variant = "main",
+            styles = {
+                transparency = true,
+            },
+        },
     },
 
     {
