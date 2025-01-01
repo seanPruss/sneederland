6365d322b175f3ec12073bb0995b474f696a4eae
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-01-01 12:47:04 -0500

    modify change-wallpaper.sh to save a bit more ram

--- config-files/.local/bin/change-wallpaper.sh
+++ config-files/.local/bin/change-wallpaper.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-cp -f $1 ~/.cache/current_wallpaper
-hyprctl hyprpaper preload "$1"
-hyprctl hyprpaper wallpaper ", $1"
+update-wallpaper() {
+	cp -f $1 ~/.cache/current_wallpaper
+	hyprctl hyprpaper preload "$1"
+	hyprctl hyprpaper wallpaper ", $1"
+}
+WALLPAPER_COUNT=$(hyprctl hyprpaper listloaded | wc -l)
+
+if ((WALLPAPER_COUNT <= 20)); then
+	update-wallpaper "$@"
+else
+	rand=$($RANDOM % 4)
+	if ((rand == 0)); then
+		update-wallpaper "$@"
+	else
+		NEW_WALLPAPER=$(hyprctl hyprpaper listloaded | shuf -n 1)
+		update-wallpaper "$NEW_WALLPAPER"
+	fi
+fi
