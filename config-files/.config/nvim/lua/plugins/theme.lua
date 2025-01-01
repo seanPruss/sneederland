ed88652f86b695b582f9eeed11dc4772607a5fff
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-01-01 09:48:59 -0500

    transparent neovim with black terminal

--- config-files/.config/nvim/lua/plugins/theme.lua
+++ config-files/.config/nvim/lua/plugins/theme.lua
@@ -2,6 +2,11 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
+        opts = {
+            styles = {
+                transparency = true,
+            },
+        },
     },
 
     {
