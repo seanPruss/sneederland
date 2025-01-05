8e41e1385e6bc17edc3b5f20527dfb25058cbbae
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-01-05 12:43:13 -0500

    get rid of ghostty shaders

--- config-files/.local/bin/change-wallpaper.sh
+++ config-files/.local/bin/change-wallpaper.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-update-wallpaper() {
-	cp -f "$1" ~/.cache/current_wallpaper
-	hyprctl hyprpaper preload "$1"
-	hyprctl hyprpaper wallpaper ", $1"
-}
-
-WALLPAPER_COUNT=$(hyprctl hyprpaper listloaded | wc -l)
-if ((WALLPAPER_COUNT <= 15)); then
-	update-wallpaper "$@"
-else
-	rand=$($RANDOM % 4)
-	if ((rand == 0)); then
-		update-wallpaper "$@"
-	else
-		NEW_WALLPAPER=$(hyprctl hyprpaper listloaded | shuf -n 1)
-		update-wallpaper "$NEW_WALLPAPER"
-	fi
-fi
+cp -f "$1" ~/.cache/current_wallpaper
+hyprctl hyprpaper preload "$1"
+hyprctl hyprpaper wallpaper ", $1"
