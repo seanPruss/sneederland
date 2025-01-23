6503ddeadb2c1689569e8ea58726c653ffb701a4
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-01-23 17:56:30 -0500

    colour scheme selector

---
+++ scripts/.local/bin/colour-scheme-selector.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+COLOUR_SCHEMES=(rose-pine catpuccin)
+
+SELECTION=$(printf '%s\n' "${COLOUR_SCHEMES[@]}" | tofi)
+
+cd $(fd -td sneederland $HOME) || exit
+
+stow --override=.* --target=$HOME "dotfiles-$SELECTION"
