a2171e0e333bb40276b0ecd271c729f4ccabd9e2
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-01-27 18:46:43 -0500

    more wallpapers

--- common/.local/bin/colour-scheme-selector.sh
+++ common/.local/bin/colour-scheme-selector.sh
@@ -4,8 +4,11 @@ COLOUR_SCHEMES=(rose-pine catppuccin)
 
 SELECTION=$(printf '%s\n' "${COLOUR_SCHEMES[@]}" | tofi --config "$HOME/.config/tofi/colour-scheme-switcher-config")
 
+[[ -z $SELECTION ]] && exit
 cd "$(fd -td sneederland $HOME)" || exit
 
+rm -rf ~/Pictures/wallpapers/*
+stow --override=.* --target=$HOME common
 stow --override=.* --target=$HOME "dotfiles-$SELECTION"
 
 case "$SELECTION" in
@@ -20,5 +23,6 @@ catppuccin)
 	;;
 esac
 
+killall hyprpaper && hyprctl dispatch exec hyprpaper
 pypr reload
 bat cache --build
