2d37f7035f6fe098c2239b681b60257fc4cae8a0
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-02-14 14:26:37 -0500

    idk why this didn

--- common/.local/bin/hyprland-restart.sh
+++ common/.local/bin/hyprland-restart.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 pgrep waybar && killall waybar
-pgrep waybar || hyprctl dispatch exec waybar
+hyprctl dispatch exec waybar
 
 pgrep dunst && killall dunst
-pgrep dunst || hyprctl dispatch exec dunst
+hyprctl dispatch exec dunst
 
 pgrep hyprpaper && killall hyprpaper
-pgrep hyprpaper || hyprctl dispatch exec hyprpaper
+hyprctl dispatch exec hyprpaper
 
 pgrep pypr && pypr reload
