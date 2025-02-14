6aaafc4fadd0a005e3b9501a90ca40101a26063d
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-02-14 13:52:47 -0500

    balls

--- common/.local/bin/hyprland-restart.sh
+++ common/.local/bin/hyprland-restart.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 pgrep waybar && killall waybar
-hyprctl dispatch exec waybar
+pgrep waybar || hyprctl dispatch exec waybar
 
 pgrep dunst && killall dunst
-hyprctl dispatch exec dunst
+pgrep dunst || hyprctl dispatch exec dunst
 
 pgrep hyprpaper && killall hyprpaper
-hyprctl dispatch exec hyprpaper
+pgrep hyprpaper || hyprctl dispatch exec hyprpaper
 
 pgrep pypr && pypr reload
