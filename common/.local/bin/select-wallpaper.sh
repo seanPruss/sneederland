2fd6cad97d8a1ccf9e8c554585a9f91c7d2427b1
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-03-11 07:55:44 -0400

    just realized I hard coded my username lol

--- common/.local/bin/select-wallpaper.sh
+++ common/.local/bin/select-wallpaper.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-WALLPAPER="$(fd . ~/Pictures/wallpapers -tf --follow | sed 's/\/home\/seanp\/Pictures\/wallpapers\///g' | tofi --config="$HOME"/.config/tofi/wallpaper-selector-config --fuzzy-match=true)"
+WALLPAPER="$(fd . ~/Pictures/wallpapers -tf --follow | tofi --config=$HOME/.config/tofi/wallpaper-selector-config --fuzzy-match=true)"
 
-WALLPAPER=$HOME/Pictures/wallpapers/$WALLPAPER
 cp -f "$WALLPAPER" ~/.cache/current_wallpaper
 hyprctl hyprpaper reload ,"$WALLPAPER"
