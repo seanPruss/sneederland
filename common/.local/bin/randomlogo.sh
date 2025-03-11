b3dc6da8076a6346ff95f599c1b6197321f56abd
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-03-11 09:13:36 -0400

    random toilet font

--- common/.local/bin/randomlogo.sh
+++ common/.local/bin/randomlogo.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 rand=$((RANDOM % 8))
+FONT=$(ls -1 /usr/share/figlet/ | sed -r '/_/d; s/\..*//' | shuf -n 1)
 
 if [ $rand -eq 0 ] || [ $rand -eq 4 ]; then
 	fastfetch
 elif [ $rand -eq 1 ] || [ $rand -eq 5 ]; then
 	colorscript random
 elif [ $rand -eq 2 ]; then
-	toilet -t -f mono9 'I use Arch btw' --rainbow
+	toilet -t -f "$FONT" 'I use Arch btw' --rainbow
 elif [ $rand -eq 3 ]; then
-	toilet -t -f mono9 'The Sneed Machine' --rainbow
+	toilet -t -f "$FONT" 'The Sneed Machine' --rainbow
 else
 	fortune | cowsay -f "$(cowsay -l | shuf -n 1)"
 fi
