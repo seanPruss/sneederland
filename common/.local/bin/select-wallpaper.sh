5864dba9f4a9a9090c8f71b4b1ade6b6bef289d0
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-03-11 08:00:55 -0400

    there we go

--- common/.local/bin/select-wallpaper.sh
+++ common/.local/bin/select-wallpaper.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-WALLPAPER="$(fd . ~/Pictures/wallpapers -tf --follow | tofi --config=$HOME/.config/tofi/wallpaper-selector-config --fuzzy-match=true)"
-
+cd ~/Pictures/wallpapers || exit
+WALLPAPER="$(fd . -tf --follow | tofi --config=$HOME/.config/tofi/wallpaper-selector-config --fuzzy-match=true)"
+WALLPAPER=$HOME/Pictures/wallpapers/$WALLPAPER
 cp -f "$WALLPAPER" ~/.cache/current_wallpaper
 hyprctl hyprpaper reload ,"$WALLPAPER"
