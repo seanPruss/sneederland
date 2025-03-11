288f66217909a505f435c707fb565d7db799ea40
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-03-11 08:30:11 -0400

    balls

--- common/.local/bin/scroll-media.sh
+++ common/.local/bin/scroll-media.sh
@@ -1,8 +1,8 @@
 CURRENT_SONG="$HOME/.local/bin/mediaplayer.sh"
 zscroll -p " | " --delay 0.2 \
-	--length 40 \
+	--length 30 \
 	--match-command "playerctl -p spotify status" \
 	--match-text "Playing" "--scroll 1" \
-	--match-text "Paused" "--before-text '󰏤 ' --scroll 0" \
+	--match-text "Paused" "--before-text '󰏤 ' --scroll 1" \
 	--update-interval 1 \
 	--update-check true $CURRENT_SONG &
