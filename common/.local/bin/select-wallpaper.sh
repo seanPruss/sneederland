e32d5866b961cfce4d146ac445a81b9191fc139f
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-04-08 15:46:02 -0400

    rofi stuff

--- common/.local/bin/select-wallpaper.sh
+++ common/.local/bin/select-wallpaper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ~/Pictures/wallpapers || exit
-WALLPAPER="$(fd . -tf --follow | rofi -demnu)"
+WALLPAPER="$(fd . -tf --follow | rofi -dmenu)"
 if [ -n "$WALLPAPER" ]; then
 	WALLPAPER=$HOME/Pictures/wallpapers/$WALLPAPER
 	cp -f "$WALLPAPER" ~/.cache/current_wallpaper
