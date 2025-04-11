e08065f6147d7be7a3f83f99c8cf26eeb80c3b38
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-04-10 21:23:28 -0400

    balls

--- common/.local/bin/manage-daemons.sh
+++ common/.local/bin/manage-daemons.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 run-gammastep() {
+	local latitude=$(curl -s https://ipinfo.io/loc | cut -d ',' -f 1)
+	local longitude=$(curl -s https://ipinfo.io/loc | cut -d ',' -f 2)
 	while true; do
-		local latitude=$(curl -s https://ipinfo.io/loc | cut -d ',' -f 1)
-		local longitude=$(curl -s https://ipinfo.io/loc | cut -d ',' -f 2)
 		pgrep gammastep || gammastep -t 6500:1500 -l "$latitude":"$longitude"
 	done
 }
 
 run-hyprpaper() {
 	while true; do
-		pgrep hyprpaper || hyprpaper
-		pypr wall next
+		pgrep hyprpaper || hyprpaper && pypr wall next
 	done
 }
 
