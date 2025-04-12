a353f7da7be8cf8870579a25434eca9a825fefcf
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-04-12 16:15:59 -0400

    nuclear option for changing gtk4 theme

--- common/.local/bin/colour-scheme-selector.sh
+++ common/.local/bin/colour-scheme-selector.sh
@@ -9,24 +9,31 @@ update-colours() {
 	stow --no-folding --override=.* --target="$HOME" "dotfiles-$1"
 	stow --no-folding --override=.* --target="$HOME" common
 
+	rm -rf ~/.config/gtk-4.0
 	case "$1" in
 	rose-pine)
 		gsettings set org.gnome.desktop.interface gtk-theme "rose-pine-gtk"
+		ln -sf /usr/share/themes/rose-pine-gtk/gtk-4.0 ~/.config
 		;;
 	catppuccin)
 		gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-red-standard+default"
+		ln -sf ~/.local/share/themes/catppuccin-mocha-red-standard+default/gtk-4.0 ~/.config
 		;;
 	dracula)
 		gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+		ln -sf /usr/share/themes/Dracula/gtk-4.0 ~/.config
 		;;
 	gruvbox)
 		gsettings set org.gnome.desktop.interface gtk-theme "Gruvbox-Red-Dark"
+		ln -sf /usr/share/themes/Gruvbox-Red-Dark/gtk-4.0 ~/.config
 		;;
 	tokyo-night)
 		gsettings set org.gnome.desktop.interface gtk-theme "Tokyonight-Dark"
+		ln -sf /usr/share/themes/Tokyonight-Dark/gtk-4.0 ~/.config
 		;;
 	nord)
 		gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
+		ln -sf /usr/share/themes/Nordic/gtk-4.0 ~/.config
 		;;
 	*)
 		echo "how tf did you get here"
