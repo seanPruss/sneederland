f35870befa59ea1859ba57bdb27f8a0c10b4014b
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-04-14 17:54:15 -0400

    keybinds menu

---
+++ common/.local/bin/hyprland-keybinds.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+keybinds=$(grep -E '^bind' ~/.config/hypr/common.conf | grep -v XF86 | grep -v bindm)
+
+# check for any keybinds to display
+if [[ -z "$keybinds" ]]; then
+	echo "no keybinds found."
+	exit 1
+fi
+
+# replace $mainmod with super in the displayed keybinds for rofi
+display_keybinds=$(echo "$keybinds" | sed 's/\$mainMod/SUPER/g' | sed 's/bind =//g' | awk -F , '{print $1 $2 $5}')
+
+# use rofi to display the keybinds with the modified content
+echo "$display_keybinds" | rofi -dmenu
