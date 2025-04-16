f84356c9cc650f2ba7ec866218fc1aa6583a7b8e
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-04-16 18:58:24 -0400

    add configuration options from repo for rainbow-delimiters

--- common/.config/nvim/lua/config/options.lua
+++ common/.config/nvim/lua/config/options.lua
@@ -27,3 +27,28 @@ opt.colorcolumn = "80"
 opt.clipboard = ""
 opt.cursorcolumn = true
 vim.g.lazyvim_picker = "snacks"
+
+---@type rainbow_delimiters.config
+vim.g.rainbow_delimiters = {
+    strategy = {
+        [""] = "rainbow-delimiters.strategy.global",
+        vim = "rainbow-delimiters.strategy.local",
+    },
+    query = {
+        [""] = "rainbow-delimiters",
+        lua = "rainbow-blocks",
+    },
+    priority = {
+        [""] = 110,
+        lua = 210,
+    },
+    highlight = {
+        "RainbowDelimiterRed",
+        "RainbowDelimiterYellow",
+        "RainbowDelimiterBlue",
+        "RainbowDelimiterOrange",
+        "RainbowDelimiterGreen",
+        "RainbowDelimiterViolet",
+        "RainbowDelimiterCyan",
+    },
+}
--- common/.config/nvim/lua/plugins/rainbow-delimiters.lua
+++ common/.config/nvim/lua/plugins/rainbow-delimiters.lua
@@ -1,5 +1,4 @@
 return {
     "HiPhish/rainbow-delimiters.nvim",
-    opts = {},
     event = { "BufReadPre", "BufNewFile" },
 }
