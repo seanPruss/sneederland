050fbe0f040a528d7982f514cbbd8c9c380838e8
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-04-22 20:16:58 -0400

    move declarations and assignments for locals

--- common/.local/bin/manage-daemons.sh
+++ common/.local/bin/manage-daemons.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 run-gammastep() {
-	local latitude=$(curl -s https://ipinfo.io/loc | cut -d ',' -f 1)
-	local longitude=$(curl -s https://ipinfo.io/loc | cut -d ',' -f 2)
+	local latitude
+	latitude=$(curl -s https://ipinfo.io/loc | cut -d ',' -f 1)
+	local longitude
+	longitude=$(curl -s https://ipinfo.io/loc | cut -d ',' -f 2)
 	while true; do
 		pgrep gammastep || gammastep -t 6500:1500 -l "$latitude":"$longitude"
 	done
