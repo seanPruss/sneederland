5554686f099e8b1bb93cd9e0a5132ea011ed9edf
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-04-25 16:46:19 -0400

    somehow this fixed waybar tooltips not showing up idk

--- common/.local/bin/scroll-media.sh
+++ common/.local/bin/scroll-media.sh
@@ -3,6 +3,6 @@ zscroll -p "   " --delay 0.2 \
 	--length 20 \
 	--match-command "playerctl -p spotify status" \
 	--match-text "Playing" "--scroll 1" \
-	--match-text "Paused" "--before-text '󰏤 ' --scroll 1" \
+	--match-text "Paused" "--before-text '󰏤 ' --scroll 0" \
 	--update-interval 1 \
 	--update-check true "$CURRENT_SONG" &
