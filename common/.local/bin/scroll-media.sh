48206971bd0bc3ada14f330e7cbd59214bfff02c
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-04-26 18:45:39 -0400

    still scroll the text when paused

--- common/.local/bin/scroll-media.sh
+++ common/.local/bin/scroll-media.sh
@@ -1,8 +1,8 @@
-CURRENT_SONG="mediaplayer.sh"
+SONG_SCRIPT="mediaplayer.sh"
 zscroll -p "   " --delay 0.6 \
 	--length 20 \
 	--match-command "playerctl -p spotify status" \
 	--match-text "Playing" "--scroll 1" \
-	--match-text "Paused" "--before-text '󰏤 ' --scroll 0" \
+	--match-text "Paused" "--before-text '󰏤 ' --scroll 1" \
 	--update-interval 1 \
-	--update-check true "$CURRENT_SONG"
+	--update-check true "$SONG_SCRIPT"
