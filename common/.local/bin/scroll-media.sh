a7dd061b0aae5a7063c167da2d54e032d4a43455
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-04-26 18:42:48 -0400

    fix tooltips not working while media is scrolling

--- common/.local/bin/scroll-media.sh
+++ common/.local/bin/scroll-media.sh
@@ -1,8 +1,8 @@
 CURRENT_SONG="mediaplayer.sh"
-zscroll -p "   " --delay 0.2 \
+zscroll -p "   " --delay 0.6 \
 	--length 20 \
 	--match-command "playerctl -p spotify status" \
 	--match-text "Playing" "--scroll 1" \
 	--match-text "Paused" "--before-text '󰏤 ' --scroll 0" \
 	--update-interval 1 \
-	--update-check true "$CURRENT_SONG" &
+	--update-check true "$CURRENT_SONG"
