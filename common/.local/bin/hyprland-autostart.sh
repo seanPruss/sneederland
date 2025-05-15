6a98770f87b527e19cefc52eb0e65f2ea6232531
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-05-15 14:04:04 -0400

    use post install script for installing plugins

--- common/.local/bin/hyprland-autostart.sh
+++ common/.local/bin/hyprland-autostart.sh
@@ -22,8 +22,12 @@ while ! ping -c 1 archlinux.org; do
 done
 
 check-repo-updates &
-hyprctl dispatch exec -- flatpak run com.spotify.Client
-hyprctl dispatch exec -- vesktop
-sleep 5
-hyprctl dispatch killactive
-hyprctl dispatch killactive
+if [ ! -e ~/.post-install ]; then
+	ghostty --class=ghostty.postinstall -e post-install.sh
+else
+	hyprctl dispatch exec -- flatpak run com.spotify.Client
+	hyprctl dispatch exec -- vesktop
+	sleep 5
+	hyprctl dispatch killactive
+	hyprctl dispatch killactive
+fi
---
+++ common/.local/bin/post-install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "One more step to complete the installation. Enter your password below to install and enable Hyprland plugins."
+hyprpm add https://github.com/hyprwm/hyprland-plugins || exit
+hyprpm add https://github.com/virtcode/hypr-dynamic-cursors || exit
+hyprpm enable hyprtrails || exit
+hyprpm enable dynamic-cursors || exit
+touch ~/.post-install
