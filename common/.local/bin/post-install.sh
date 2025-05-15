e803ea1ade3c2cdd59f0f2d36972bb10b56de70a
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-05-15 15:25:35 -0400

    need to update hyprpm post install

--- common/.local/bin/post-install.sh
+++ common/.local/bin/post-install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "One more step to complete the installation. Enter your password below to install and enable Hyprland plugins."
+hyprpm update -v -s
 hyprpm add https://github.com/hyprwm/hyprland-plugins || exit
 hyprpm add https://github.com/virtcode/hypr-dynamic-cursors || exit
 hyprpm enable hyprtrails || exit
