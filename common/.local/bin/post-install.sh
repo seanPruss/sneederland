8b092627eb6beff2bb05dcdcc5e4af2026d3b00f
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-06-02 16:02:12 -0400

    start ignition with uwsm

--- common/.local/bin/post-install.sh
+++ common/.local/bin/post-install.sh
@@ -7,7 +7,7 @@ hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
 hyprpm enable hyprtrails
 hyprpm enable dynamic-cursors
 echo "You can also autostart apps with ignition"
-flatpak run io.github.flattool.Ignition &
+hyprctl dispatch exec -- uwsm-app -- flatpak run io.github.flattool.Ignition
 touch ~/.post-install
 tput setaf 5 bold
 read -rep "Press any key to exit" -s -n 1
