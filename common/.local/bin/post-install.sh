4a2a2ce748b09717a482f853f04ab0beb7b0cd0c
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-07-25 19:20:03 -0400

    add stuff to post install script

--- common/.local/bin/post-install.sh
+++ common/.local/bin/post-install.sh
@@ -8,7 +8,9 @@ hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
 hyprpm enable hyprtrails || exit
 hyprpm enable hyprfocus || exit
 hyprpm enable dynamic-cursors || exit
+clear
 echo "You can also autostart apps with ignition"
+echo "Press SUPER+/ for keybindings"
 hyprctl dispatch exec -- uwsm-app -- flatpak run io.github.flattool.Ignition
 touch ~/.post-install
 tput setaf 5 bold
