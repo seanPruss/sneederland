cf6209d0e67acea16a03e6bb3f167c080b0b630d
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-10-20 10:30:49 -0400

    adjust hyprpaper script cuz idk if the previous one would have worked

--- common/.local/bin/hyprpaper-service.sh
+++ common/.local/bin/hyprpaper-service.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-while ! systemctl --user enable --now hyprpaper.service; do
-	true
+while ! systemctl --user status hyprpaper.service; do
+	systemctl --user enable --now hyprpaper.service
 done
