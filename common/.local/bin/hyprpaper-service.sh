b4f407c09c6fa21c357a13e582f4761fe06eb804
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-11-04 17:41:31 -0500

    kill hyprpaper if it's bugging out

--- common/.local/bin/hyprpaper-service.sh
+++ common/.local/bin/hyprpaper-service.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+killall hyprpaper
 systemctl --user enable --now hyprpaper.service
 while ! systemctl --user status hyprpaper.service; do
 	systemctl --user enable --now hyprpaper.service
