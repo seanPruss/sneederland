f8066b0449eb33793a6e05eeb5348c502e54f6d7
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-11-05 11:37:42 -0500

    trying this

--- common/.local/bin/hyprpaper-service.sh
+++ common/.local/bin/hyprpaper-service.sh
@@ -3,9 +3,9 @@
 head -n 4 ~/.local/state/waypaper/state.ini >~/.local/state/waypaper/state.ini.temp
 cat ~/.local/state/waypaper/state.ini.temp >~/.local/state/waypaper/state.ini
 rm ~/.local/state/waypaper/state.ini.temp
+waypaper --random
 killall hyprpaper
 systemctl --user enable --now hyprpaper.service
 while ! systemctl --user status hyprpaper.service; do
 	systemctl --user enable --now hyprpaper.service
 done
-waypaper --random
