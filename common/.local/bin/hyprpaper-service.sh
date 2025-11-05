19dc3429cee05180b82aac349222d5250f2b8f9c
seanPruss <36770417+seanPruss@users.noreply.github.com>
2025-11-05 11:34:06 -0500

    make sure waypaper state.ini file isn't wrong

--- common/.local/bin/hyprpaper-service.sh
+++ common/.local/bin/hyprpaper-service.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+head -n 4 ~/.local/state/waypaper/state.ini >~/.local/state/waypaper/state.ini.temp
+cat ~/.local/state/waypaper/state.ini.temp >~/.local/state/waypaper/state.ini
+rm ~/.local/state/waypaper/state.ini.temp
 killall hyprpaper
 systemctl --user enable --now hyprpaper.service
 while ! systemctl --user status hyprpaper.service; do
