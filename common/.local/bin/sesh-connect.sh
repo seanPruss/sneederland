95898c383c5292832499e8aa4360003191c1ea67
seanPruss <36770417+seanPruss@users.noreply.github.com>
2026-02-03 09:49:32 -0500

    this better work lmao

--- common/.local/bin/sesh-connect.sh
+++ common/.local/bin/sesh-connect.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sesh connect "$(
-	sesh list --hide-attached --icons | fzf --reverse \
+	sesh list --icons | fzf --reverse \
 		--preview-window 'right:70%' \
 		--preview 'sesh preview {}' \
 		--no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
