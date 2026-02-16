53a4bf167e1cc5b7bf94d6bea715027ab9d73445
seanPruss <36770417+seanPruss@users.noreply.github.com>
2026-02-15 21:22:23 -0500

    don't need this

--- common/.local/bin/dms-niri-screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-OPTION=$(echo -e "screenshot\nscreenshotScreen\nscreenshotWindow" | tofi)
-
-[[ -n $OPTION ]] || exit
-dms ipc call niri "$OPTION"
-sleep 7
-wl-paste | satty --filename -
